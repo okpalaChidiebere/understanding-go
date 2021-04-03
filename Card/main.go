99d503b6fb8218bea2b55031c106f85ca6a3f11d
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-03 08:13:41 -0600

    Variable assignment - 2
    Go Playground at https://play.golang.org/ to quickly run a snippet of code.

--- Card/main.go
+++ Card/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//card := "Ace of Spades" this will give a runtime error.Variables can be initialized outside of a function, but cannot be assigned a value.
+//Eg: var deckSize int  is valid. We can now later assign value to it inside the main function.
 func main() {
 	/*
 		using the keyword "var" we are telling go compiler we are creating a variable
@@ -13,6 +15,14 @@ func main() {
 	//var card string = "Ace of Spades" //long format way of declaring a varibale
 	card := "Ace of Spades"   //shortHand way of declaring a varibel in go. We only use ":=" when we are initializing a new variable
 	card = "Five of Diamonds" //we change the value of card. Notice that we did not use the ":" symbole anymore for re-assigning
+
+	/*
+			We might be able to initialize a variable and then later assign a variable to it
+
+			Eg:
+			var deckSize int
+		    deckSize = 52
+	*/
 
 	fmt.Println(card)
 }
