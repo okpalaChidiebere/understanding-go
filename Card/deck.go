2bfdaff3e2ce3eaf48f68ec900353ef54dc6f643
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-04 09:33:51 -0600

    Multiple Return values from thesame function
    We learned How to use the Range Syntax with a Splice to Split a slice into two as well

--- Card/deck.go
+++ Card/deck.go
@@ -40,6 +40,33 @@ func (d deck) printCards() { //NOTE: the "(d deck)" is a receiver.
 }
 
 /*
+The idea behind the deal method is that we take an existing deck of cards,
+we will take a number of cards to deal out and we will create a new deck of
+the specified size. This will simulate having a deck of 52 cards in practise
+and dealing out a hand of 5 to 7 cards. Whatever size you want.
+
+Eg we have deck that has 4 cards, and we specify that we want  deals(3)(3 cards in our hands)
+and 1 of our card on the deck, it will return two two decks of one that contains
+3 cards and a deck that contain one card
+
+If we want to select a range within a specific slice
+fruits[startingIndexIncluding : upToAndNotIncluding]
+Eg assume we have fruits["apple", "banana", "orange", "mango"]
+fruits[0:2] or fruits[:2] gives us fruits["apple", "banana"]
+fruits[2:] gives us fruits["orange", "mango"]
+
+First argument: is our deck. Remember naming convention for go is to have the 1 to 3 characters for the actual variable.
+There is no this or self keyword in go
+
+Second Argument: is amount of cards we want in our hands that we want to return
+
+Return value: The function returns two values of type deck. In go you can tell the compiler that we are returning two values. C++ has this functionality as well
+*/
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:] //we return the first deck which is the one in our hand, and the second value is the remaining cards on deck
+}
+
+/*
 Go is not an object oriented programming Language like C++, Java, Python. So, there is no idea of classes inside of Go
 
 Eg in Real OOP, we could have a Deck class of which we can create an instance of that Deck class where we can have some
--- Card/main.go
+++ Card/main.go
@@ -4,6 +4,13 @@ func main() {
 
 	cards := newDeck() //REMEMBER: we can call this method in the deck.go file from this file, because main.go and deck.go belongs to thesame package "main"!
 
+	hand, remainingCards := deal(cards, 5) //we defined two separate variables to receive the two separate values that is returned by this function
+	//the first return value will be assinged to 'hand', and the second return value will be assigned to 'remainingCards'. Both values of type deck\
+
+	//REMEMBER: you can call the print function on this two variables because they are of type deck
+	hand.printCards()
+	remainingCards.printCards()
+
 	/*
 	  we can now call the printCard() method on the cards variable. This is because of the receiver we set up on it
 
