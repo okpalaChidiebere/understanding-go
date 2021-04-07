5c1de9185c268ae0b5fe4adbc5fe9863c22c64ee
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-06 18:58:47 -0600

    How to Read documentation to understand how to identify & use interfaces in the standard library
    - We learn about the Reader and Writer interface
    
    These two interface are quite common to that you will encounter.
    
    Reader interface is one of the most common interface you will encounter in go. The purpose of this interface is to put an abstraction over all different type of input(from http, hard drive, commandLine, analogSensor plugged into a machine, etc) into our application. Basically helps but read input from whatever source and converts it into bytes in our code. Then we can read the bytes ourselves in our code. Instead of having separate different functions for different types of input, Reader interface abstracts all of them to one!
    
    The Writer interface is used to take some data within our program(in bytes) and send that data out to some source of output(like terminal, outgoing HTTP request, text file, image file). It basically does the reverse of the Reader interface

---
+++ http/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	/*
+		Having this is not enough. We could not find the body of the response.
+		In other programming languages like JS this would be fine. But with go
+		its different. We need the help of interfaces
+
+		Looking at the documentation https://golang.org/pkg/net/http/#Response, we were logging out a struct
+
+		The body is of type ReadCloser. If it were js, the body would be a JSON or a blob of string
+		We now dig into https://golang.org/pkg/io/#ReadCloser to see its an interface!
+		The ReadCloser implements the Reader interface
+	*/
+	fmt.Println(resp)
+
+	/*
+		Here we are mainig use of the Read function implemented by the Reader interface to get the bytes raw data that gets converted to bytes
+
+		if you look at this https://golang.org/pkg/io/#ReadCloser you will notice that the Reader interface(https://golang.org/pkg/io/#Reader) is inside ReadCloser interfce.
+		Yes, it is because in go, we can wrap an interface inside an interface!
+
+		Because the ReaderCLoser implemets the Reader interface, it is have access to call the Read() method
+	*/
+	bs := make([]byte, 99999) //We make a asummtion 99999 spaces or elements is big enough to accomodate the data that is returned by our Reader. If we initialize a byte slice with 0 elements inside of it, the Reader will assume the reader is full and will just quit early
+	resp.Body.Read(bs)        //We get the raw html from the body field and read that into our byte slice
+	fmt.Println(string(bs))   //We converted our bytes to string because the commandline cannot write bytes
+
+	/*
+		The process of which we declare a space for bytes, read bytes convert it to string to print to the terminal we dont always have to do
+		Go had built in helper functions to do this for us and it is the Copy() function https://golang.org/pkg/io/#Copy
+
+		hold command button and click on the Copy() function to see for yourself its implementation in your IDE
+
+		Ths Copy function expects the
+		- first argument to be a value that implements the Writer interface. Rememebr the writer interface takes data and sends it outside of our application
+		- The second argument to be a value that implements the Reader inteface
+		In summary the Copy function helps takes data from inside of our application and write or copy the information to outside of our app. Basically like the three lines of code above
+
+		//os.StdOut variable is of type File that implements the Write interface because the File type has a function called Write() that receive byte slice and returns an int or error https://golang.org/pkg/io/#Writer
+		//resp.Body varibale implements the Read interface already
+
+
+	*/
+	io.Copy(os.Stdout, resp.Body)
+}
