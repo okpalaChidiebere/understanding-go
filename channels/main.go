ad4ce55718996d66d338f871e21bce870c8f77ee
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-09 16:43:43 -0600

    Function Literals in GO. Similar to Anonymous function in JavaScript
    Problem
    We want to continually ping the sites url every 5 seconds
    
    The sleep() function is a blocking code.
    
    For this exercise,
    - we want to avoid putting a pause on our main routine (Main thread). When the main routine is paused it cannot receive any other messages through the channel even though the go or child routines (background threads) could be done and send messages back to the main thread through the channel.
    - We also want to avoid calling the sleep function in the checkLinkAsync() method as well because as a developer when we call that method, we expects it to check the link immediately and not pause for 5 seconds because doing so
    
    Solution
    We want to have this sleep function in  between our Main routine and the checkLinsAsync function. We solve this by having a function Literal run in the background thread where we can now have our sleep function as well as our checkLinkAsync() function call logic. This way we are successfully checking the status of these websites every 5 seconds
    
    Function Literal is an un-named function that we can use to wrap some little chunk of code so that we can execute it at some point in time.

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,8 +22,17 @@ func main() {
 	}
 
 	for l := range c { //so now this for loop will wait for next next channel value. The range c is now a Block code
-		//we refetch the link being fetched again
-		go checkLinkAsync(l, c) //first argument will be the link that the previous routine returned(go know the type fo value returned by the channel is a string), the second argument will be the channel
+		//we run our function literal in a go routine (background theread)
+		go func(link string) { //We get all the arguments passed into a function literal here. In our case, only the link returned through the channel was passed
+			time.Sleep(5 * time.Second)
+			checkLinkAsync(link, c) //first argument will be the link that the previous routine returned(go know the type fo value returned by the channel is a string), the second argument will be the channel
+		}(l) /* You have to place an extra set of parenthesis to actually invoke it
+
+		Notice that we passed the link message returned through the channel into the parenthesis. This is a way to pass argument into your function literal in go(Remember this is bass by value which is default in go). You can relate to this as the way React hooks works.
+		This is necessay because anytime we are creating a child routine we want to pass the value from the main routine into the function that will run as a child routine
+
+		If we did not pass the l into the function and just call it directly from the checkLinkAsync method, Go will give us a warning that we are referecning to a global variable. We never want to point to thesame variable from different child routine. This is a NO NO
+		*/
 	}
 
 }
